0ba893f6ba23e21733f9ff6c0c0e5de53a7624c1
761105568@qq.com <dadong>
2016-09-07 13:33:55 +0800

    全部导入git

---
+++ WechatPicLooker/src/main/java/com/example/imagedemo/ImagePagerActivity.java
@@ -0,0 +1,94 @@
+package com.example.imagedemo;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.Window;
+import android.widget.TextView;
+
+/**
+ * 图片查看器
+ */
+public class ImagePagerActivity extends FragmentActivity {
+    private static final String STATE_POSITION = "STATE_POSITION";
+    public static final String EXTRA_IMAGE_INDEX = "image_index";
+    public static final String EXTRA_IMAGE_URLS = "image_urls";
+
+    private HackyViewPager mPager;
+    private int pagerPosition;
+    private TextView indicator;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.image_detail_pager);
+
+        // 点的是GridView的哪个条目
+        pagerPosition = getIntent().getIntExtra(EXTRA_IMAGE_INDEX, 0);
+        ArrayList<String> urls = getIntent().getStringArrayListExtra(EXTRA_IMAGE_URLS);
+
+        mPager = (HackyViewPager) findViewById(R.id.pager);
+        ImagePagerAdapter mAdapter = new ImagePagerAdapter(getSupportFragmentManager(), urls);
+        mPager.setAdapter(mAdapter);
+        mPager.setpagerCount(urls.size());
+        indicator = (TextView) findViewById(R.id.indicator);
+
+        CharSequence text = getString(R.string.viewpager_indicator, 1, mPager.getAdapter().getCount());
+        indicator.setText(text);
+        // 更新下标
+        mPager.setOnPageChangeListener(new OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                CharSequence text = getString(R.string.viewpager_indicator, position + 1, mPager.getAdapter().getCount());
+                indicator.setText(text);
+                mPager.setCurrentIndex(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+        if (savedInstanceState != null) {
+            pagerPosition = savedInstanceState.getInt(STATE_POSITION);
+        }
+
+        mPager.setCurrentItem(pagerPosition);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt(STATE_POSITION, mPager.getCurrentItem());
+    }
+
+    private class ImagePagerAdapter extends FragmentStatePagerAdapter {
+
+        public ArrayList<String> fileList;
+
+        public ImagePagerAdapter(FragmentManager fm, ArrayList<String> fileList) {
+            super(fm);
+            this.fileList = fileList;
+        }
+
+        @Override
+        public int getCount() {
+            return fileList == null ? 0 : fileList.size();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            String url = fileList.get(position);
+            return ImageDetailFragment.newInstance(url);
+        }
+
+    }
+}
